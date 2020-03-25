558161182ad9e61d43801786ceeff7260af2974e
y-a2000 <ya48827jb4.sonet.ne.jp@gmail.com>
2020-03-25 14:12:58 +0900

    3/25

---
+++ FiveSense/Assets/KeyScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+    public bool HavingKey;
+    public int DoorCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HavingKey = false;
+        DoorCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Key"&&HavingKey==false)
+        {
+            HavingKey = true;
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "Door"&&HavingKey==true)
+        {
+            HavingKey = false;
+            DoorCount += 1;
+            if (DoorCount >= 4)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
